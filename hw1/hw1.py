fce92471275b3788ab99cca9ce40ed0ab2bdec97
Erko21 <ekedesh@gmail.com>
2020-12-25 01:01:40 +0200

    Homework1_1

---
+++ hw1/hw1.py
@@ -0,0 +1,56 @@
+class Animals():
+    def __init__(self, anymal_type, name, age):
+        self.anymal_type = anymal_type
+        self.name = name
+        self.age = age
+
+    def my_animal(self):
+        print(f"I have a {self.anymal_type} which is called {self.name.title()} and it is {self.age} years old!")
+
+class Dog(Animals):
+    def voice(self):
+        print(f"My {self.anymal_type} can bark really loud)")
+
+class Fly(Animals):
+    def __init__(self,anymal_type, name , age , wing_size):
+        super().__init__(anymal_type, name , age)
+        self.anymal_type = anymal_type
+        self.name = name
+        self.age = age
+        self.wing_size = wing_size
+
+    def wings(self):
+        print(f"My {self.anymal_type} can fly very height and it has {self.wing_size} santimeters wings!")
+
+class Restoration(Animals):
+    def wizzard_repair(self):
+        print(f"My {self.anymal_type} can repair its legs only in one day!Can u imagine that?")
+
+class Frog(Animals):
+    def __init__(self, anymal_type, name, age, full_length, body_length):
+        super().__init__(anymal_type, name, age)
+        self.full_length = full_length
+        self.body_length = body_length
+
+    def legs_length(self):
+        length = self.full_length - self.body_length
+        print(f"My {self.anymal_type} has {length} santimeters legs!")
+
+# m_anymal = Animals('dog', 'petty', '16')
+# m_anymal.my_animal()
+#
+# dog = Dog('dog', 'petty', '10')
+# dog.my_animal()
+# dog.voice()
+
+# fly = Fly('bird', 'carol', '4', '30')
+# fly.my_animal()
+# fly.wings()
+
+# wizzard = Restoration('wizzard', 'blow', '1')
+# wizzard.my_animal()
+# wizzard.wizzard_repair()
+
+frog = Frog('frog', 'carol', 1 , 25, 19)
+frog.my_animal()
+frog.legs_length()
